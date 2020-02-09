b3b2df5eabd225095c9b962f943cb3667fcecec2
Anurag-Aggarwal <44736106+Anurag-Aggarwal@users.noreply.github.com>
2020-02-09 22:45:02 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
   name = '101703088-outlier',         # How you named your package folder (MyLib)
-  packages = ['outlier'],   # Chose the same as "name"
+  packages = ['outliers'],   # Chose the same as "name"
   version = '1.0.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A Python package to detect outliers.',   # Give a short description about your library
